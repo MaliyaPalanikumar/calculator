101037d1b3d9c04b601a0a4fc5127740f6118c5d
Maliya <maliyapalanikumar@gmail.com>
2025-06-30 21:14:26 +0530

    files added

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Maliya Palanikumar on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
